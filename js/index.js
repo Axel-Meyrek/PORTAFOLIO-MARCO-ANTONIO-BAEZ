5991de941a2786b3b71b1192d8d3b7718c352548
Axel Meyrek Mac <chavezsud1406@gmail.com>
2025-02-05 15:24:20 -0600

    Function render constancias

--- js/index.js
+++ js/index.js
@@ -1,169 +1,6 @@
-const certificaciones = [
-    {
-        "img": "img/constancias/constancia-1.jpg",
-        "title": "Diplomado en Seguridad Nacional",
-        "zone": "Academia Nacional de Seguridad"
-    },
-    {
-        "img": "img/constancias/constancia-2.jpg",
-        "title": "Certificación en Ciberseguridad",
-        "zone": "Instituto de Ciberdefensa"
-    },
-    {
-        "img": "img/constancias/constancia-3.jpg",
-        "title": "Curso de Inteligencia Artificial",
-        "zone": "Universidad Tecnológica"
-    },
-    {
-        "img": "img/constancias/constancia-4.jpg",
-        "title": "Diplomado en Redes",
-        "zone": "Academia de Tecnología"
-    },
-    {
-        "img": "img/constancias/constancia-5.jpg",
-        "title": "Curso de Programación Avanzada",
-        "zone": "Centro de Estudios Informáticos"
-    },
-    {
-        "img": "img/constancias/constancia-6.jpg",
-        "title": "Curso de Análisis de Datos",
-        "zone": "Escuela de Estadística Aplicada"
-    },
-    {
-        "img": "img/constancias/constancia-7.jpg",
-        "title": "Certificación en Desarrollo Web",
-        "zone": "Academia de Software Moderno"
-    },
-    {
-        "img": "img/constancias/constancia-8.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-9.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-10.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-11.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-12.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-13.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-14.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-15.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-16.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-17.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-18.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-19.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-20.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-21.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-22.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-23.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-24.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-25.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-26.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-27.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-28.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-29.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-30.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-31.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-    {
-        "img": "img/constancias/constancia-32.jpg",
-        "title": "Diplomado en Criptografía",
-        "zone": "Centro Nacional de Seguridad Informática"
-    },
-]
-
-
-
 /* VARIABLES */
+
+let certificados = [];
 
 let indiceActual = 0;
 
@@ -173,21 +10,37 @@ const $contenedor = document.querySelector('#contenedorCertificaciones');
 
 const $buttonCargar = document.querySelector('#buttonCargar');
 
-/* FUNCIONES */ 
+/* FUNCIONES */
+
+const cargarDatos = async () => {
+    try {
+        const response = await fetch('../certificados.json');
+
+        const data = await response.json();
+
+        certificados = data;
+
+        mostrarCertificaciones();
+        
+    } catch (error) {
+        console.error('Hubo un problema al cargar los certificados:', error);
+        $contenedor.innerHTML = '<p>Error al cargar las certificaciones. Inténtalo más tarde.</p>';
+    }
+}
 
 function mostrarCertificaciones() {
     const fragmento = document.createDocumentFragment();
 
-    for (let i = indiceActual; i < indiceActual + cantidadPorCarga && i < certificaciones.length; i++) {
-        const cert = certificaciones[i];
+    for (let i = indiceActual; i < indiceActual + cantidadPorCarga && i < certificados.length; i++) {
+        const cert = certificados[i];
 
         const articulo = document.createElement('article');
         articulo.classList.add('certificacion');
 
         articulo.innerHTML = `
-            <img class="certificacion_img" src="${cert.img}" loading="lazy" alt="${cert.title}">
-            <h3 class="certificacion_title">${cert.title}</h3>
-            <p class="certificacion_zone">${cert.zone}</p>
+            <img class="certificacion_img" src="../img/constancias/constancia-${i+1}.jpg" loading="lazy" alt="${cert.nombre}">
+            <h3 class="certificacion_title">${cert.nombre}</h3>
+            <p class="certificacion_zone">${cert.egresado}</p>
         `;
 
         fragmento.appendChild(articulo);
@@ -197,14 +50,13 @@ function mostrarCertificaciones() {
     indiceActual += cantidadPorCarga;
 
     // Ocultar el botón si no hay más certificaciones
-    if (indiceActual >= certificaciones.length) {
-        botonCargarMas.style.display = 'none';
+    if (indiceActual >= certificados.length) {
+        $buttonCargar.style.display = 'none';
     }
 }
 
 /* EVENTOS */
 
-document.addEventListener('DOMContentLoaded', mostrarCertificaciones);
+document.addEventListener('DOMContentLoaded', cargarDatos);
 
 $buttonCargar.addEventListener('click', mostrarCertificaciones);
-
